ede04b698d54452830a6489fa85461179bc85794
tawatchai.w <tawatchai.w@nextcorp.co.th>
2018-09-30 21:08:58 +0700

    update requestOrder

--- functions/index.js
+++ functions/index.js
@@ -29,6 +29,49 @@ exports.LineBot = functions.https.onRequest((req, res) => {
         })
     reply(req.body);
 });
+exports.reqOrder = functions.https.onRequest((req, res) => {
+    const request = req.body.events[0];
+    if (request.message.type !== 'text' || request.source.type !== 'group') {
+        return;
+    }
+    // const data = initMsg(request.message.text);
+    const data = Object.assign(...request.message.text.split('#').filter(f => f != "")
+        .map(m => {
+            const dontReplces = ["name", "fb", "bank", "addr"];
+            const key = m.split(':')[0];
+            let value = m.split(':')[1];
+            if (!dontReplces.includes(key)) value = value.replace(/\s/g, '');
+            if (key !== 'addr') value = value.replace(/\n/g, '');
+            if (key == 'tel') value = value.replace(/\D/g, ''); //เหลือแต่ตัวเลข
+            if (key !== 'price') {
+                value = value.trim();
+                if (key == 'product') {
+                    value = value.split(',').map(p => {
+                        return {
+                            code: p.split('=')[0],
+                            amount: Number(p.split('=')[1])
+                        }
+                    });
+                }
+            } else {
+                value = Number(value);
+            }
+            return { [key]: value };
+        }));
+    const text = `รายการสั่งซื้อของคุณคือ
+    คุณ ${data.name} ${data.tel}
+    ที่อยู่: ${data.addr}
+    สินค้า: ${data.product.map(p => p.code + ' จำนวน ' + p.amount + 'ชิ้น ')}
+    ธนาคาร: ${data.bank} จำนวนเงิน ${data.price}
+    FB: ${data.fb}
+    `
+    const obj = {
+        replyToken: request.replyToken,
+        text
+    };
+
+    reply2(obj);
+});
 const reply = (bodyResponse) => {
     return request({
         method: `POST`,
@@ -49,3 +92,44 @@ const reply = (bodyResponse) => {
         })
     });
 };
+const reply2 = (obj) => {
+    return request({
+        method: `POST`,
+        uri: `${LINE_MESSAGING_API}/reply`,
+        headers: LINE_HEADER,
+        body: JSON.stringify({
+            replyToken: obj.replyToken,
+            messages: [
+                {
+                    type: `text`,
+                    text: obj.text
+                }
+            ]
+        })
+    });
+};
+const initMsg = (txt) => {
+    return Object.assign(...txt.split('#').filter(f => f != "")
+        .map(m => {
+            const dontReplces = ["name", "fb", "bank", "addr"];
+            const key = m.split(':')[0];
+            let value = m.split(':')[1];
+            if (!dontReplces.includes(key)) value = value.replace(/\s/g, '');
+            if (key !== 'addr') value = value.replace(/\n/g, '');
+            if (key == 'tel') value = value.replace(/\D/g, ''); //เหลือแต่ตัวเลข
+            if (key !== 'price') {
+                value = value.trim();
+                if (key == 'product') {
+                    value = value.split(',').map(p => {
+                        return {
+                            code: p.split('=')[0],
+                            amount: Number(p.split('=')[1])
+                        }
+                    });
+                }
+            } else {
+                value = Number(value);
+            }
+            return { [key]: value };
+        }));
+}
